8731c037a4bd97729203b71294500260c2ac9caf
MinhNghia217 <trantruongminhnghia12345@gmail.com>
2023-10-12 23:10:31 +0700

    .

---
+++ Project D/Assets/MAIN/Scrips/Menu/LevelManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] btnGameObjects;
+    [SerializeField]
+    private GameObject levelButtons;
+    [SerializeField]
+    private Sprite iconLock;
+    [SerializeField]
+    private Sprite iconUnlock;
+
+    private void Awake()
+    {
+        ButtonsToArray();
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for(int i = 0; i < btnGameObjects.Length; i++)
+        {
+            btnGameObjects[i].GetComponent<Button>().interactable = false;
+            btnGameObjects[i].transform.GetChild(0).GetComponent<Image>().sprite = iconLock;
+        }
+        for(int i = 0; i < unlockedLevel; i++)
+        {
+            btnGameObjects[i].GetComponent<Button>().interactable = true;
+            btnGameObjects[i].transform.GetChild(0).GetComponent<Image>().sprite = iconUnlock;
+        }
+    }
+    public void OpenLevel(int levelId)
+    {
+        string levelName = "Level " + levelId;
+        SceneManager.LoadScene(levelName);  
+    }
+
+    void ButtonsToArray()
+    {
+        int childCount = levelButtons.transform.childCount;
+        btnGameObjects = new GameObject[childCount];
+        for(int i = 0;i < childCount;i++)
+        {
+            btnGameObjects[i] = levelButtons.transform.GetChild(i).gameObject;
+        }
+    }
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
+
---
+++ Project D/Assets/MAIN/Scrips/Menu/MenuController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public static MenuController instance;
+
+    public event EventHandler OnPhaseChanged;
+
+    public Animator animator;
+
+    // Enum liệt kê các giai đoạn của Scene menu
+    public enum MenuPhase
+    {
+        Logo,       // Giai đoạn hiển thị logo
+        Animation,  // Giai đoạn hiển thị đoạn animation
+        Menu  // Giai đoạn hiển thị menu button
+    }
+
+    // Thời gian hiển thị của từng giai đoạn
+    public float logoDuration = 3f;
+    public float animationDuration = 17f;
+
+    // Giai đoạn hiện tại
+    private MenuPhase currentPhase;
+
+    private void Awake()
+    {
+        instance = this;
+
+        currentPhase = MenuPhase.Logo;
+    }
+
+    private void Update()
+    {
+        switch (currentPhase)
+        {
+            case MenuPhase.Logo:
+                logoDuration -= Time.deltaTime;
+                if (logoDuration < 0)
+                {
+                    currentPhase = MenuPhase.Animation;
+                    OnPhaseChanged?.Invoke(this, new EventArgs());
+                }
+                break;
+            case MenuPhase.Animation:
+                animationDuration -= Time.deltaTime;
+                if (animationDuration < 0)
+                {
+                    currentPhase = MenuPhase.Menu;
+                    OnPhaseChanged?.Invoke(this, new EventArgs());
+                }
+                break;
+            case MenuPhase.Menu:
+                animator.SetTrigger("Idle");
+                break;
+        }
+    }
+
+    public bool isLogoIntro()
+    {
+        return currentPhase == MenuPhase.Logo;
+    }
+
+    public bool isAnimation ()
+    {
+        return currentPhase == MenuPhase.Animation;
+    }
+
+    public bool isMenu()
+    {
+        return currentPhase == MenuPhase.Menu;
+    }
+}

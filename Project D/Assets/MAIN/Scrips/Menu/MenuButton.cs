94c7aa2c8020dade37d2acfcac3775eb6587f552
MinhNghia217 <trantruongminhnghia12345@gmail.com>
2023-10-12 23:24:24 +0700

    .

--- Project D/Assets/MAIN/Scrips/Menu/MenuButton.cs
+++ Project D/Assets/MAIN/Scrips/Menu/MenuButton.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
+    public void changeLevelMap()
+    {
+        SceneManager.LoadScene("LevelMap");
+    }
 
 }

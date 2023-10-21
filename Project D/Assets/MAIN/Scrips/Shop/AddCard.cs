79cc0b05af64d263d6bb6d7de69b4d704e4da636
MinhNghia217 <trantruongminhnghia12345@gmail.com>
2023-10-21 23:59:58 +0700

    .

--- Project D/Assets/MAIN/Scrips/Shop/AddCard.cs
+++ Project D/Assets/MAIN/Scrips/Shop/AddCard.cs
@@ -10,62 +10,78 @@ public class AddCard : MonoBehaviour
 {
     public static AddCard Instance { get; private set; }
 
-    public List<GameObject> cardMeleeButtons, cardMageButtons, cardArcherButtons;
+    public List<GameObject> meleeCards, mageCards, archerCards, skillCards;
 
     [SerializeField]
-    private Transform meleeContent, mageContent, archerContent;
+    private Transform meleeContent, mageContent, archerContent, skillContent;
 
     [SerializeField]
-    private GameObject meleeCardPrefeb, mageCardPrefeb, archerCardPrefeb;
+    private GameObject meleeCardPrefab, mageCardPrefab, archerCardPrefab, skillCardPrefab;
 
     public List<SO_CharacterInforMantion> meleeSOList;
     public List<SO_CharacterInforMantionRANGER> archerSOList;
     public List<SO_CharacterInforMantionRANGER> mageSOList;
+    public List<SO_Active_Skill> skillSOList;
 
     private void Awake()
     {
         Instance = this;
-        cardMeleeButtons = new List<GameObject>();
-        cardMageButtons = new List<GameObject>();
-        cardArcherButtons = new List<GameObject>();
+        meleeCards = new List<GameObject>();
+        mageCards = new List<GameObject>();
+        archerCards = new List<GameObject>();
+        skillCards = new List<GameObject>();
 
-        meleeSOList = new List<SO_CharacterInforMantion>(Resources.LoadAll<SO_CharacterInforMantion>("SO_Axie/Melee"));
-        mageSOList = new List<SO_CharacterInforMantionRANGER>(Resources.LoadAll<SO_CharacterInforMantionRANGER>("SO_Axie/Mage"));
-        archerSOList = new List<SO_CharacterInforMantionRANGER>(Resources.LoadAll<SO_CharacterInforMantionRANGER>("SO_Axie/Archer"));
+        meleeSOList = new List<SO_CharacterInforMantion>(Resources.LoadAll<SO_CharacterInforMantion>("SO_Shop/Melee"));
+        mageSOList = new List<SO_CharacterInforMantionRANGER>(Resources.LoadAll<SO_CharacterInforMantionRANGER>("SO_Shop/Mage"));
+        archerSOList = new List<SO_CharacterInforMantionRANGER>(Resources.LoadAll<SO_CharacterInforMantionRANGER>("SO_Shop/Archer"));
+        skillSOList = new List<SO_Active_Skill>(Resources.LoadAll<SO_Active_Skill>("SO_Shop/Skill"));
     }
 
     private void Start()
     {
-        Debug.Log(TabController.instance.currentTab);
         AddCardByTab();
     }
 
     private void AddCardByTab()
     {
+        //Add Melee Card
         for (int i = 0; i < meleeSOList.Count; i++)
         {
-            GameObject cardButton = Instantiate(meleeCardPrefeb);
+            GameObject cardButton = Instantiate(meleeCardPrefab);
             cardButton.name = "Melee_Card " + i;
             cardButton.transform.SetParent(meleeContent, false);
             cardButton.GetComponent<AddValue_Melee>().axieSO = meleeSOList[i];
-            cardMeleeButtons.Add(cardButton);
+            meleeCards.Add(cardButton);
         }
 
+        //Add Archer Card
         for (int i = 0; i < mageSOList.Count; i++)
         {
-            GameObject cardButton = Instantiate(mageCardPrefeb);
+            GameObject cardButton = Instantiate(mageCardPrefab);
             cardButton.name = "Mage_Card " + i;
             cardButton.transform.SetParent(mageContent, false);
             cardButton.GetComponent<AddValue_ArcherOrMelee>().axieRANGERSO = mageSOList[i];
-            cardMageButtons.Add(cardButton);
+            mageCards.Add(cardButton);
         }
+
+        //Add Mage Card
         for (int i = 0; i < archerSOList.Count; i++)
         {
-            GameObject cardButton = Instantiate(archerCardPrefeb);
+            GameObject cardButton = Instantiate(archerCardPrefab);
             cardButton.name = "Archer_Card " + i;
             cardButton.transform.SetParent(archerContent, false);
             cardButton.GetComponent<AddValue_ArcherOrMelee>().axieRANGERSO = archerSOList[i];
-            cardArcherButtons.Add(cardButton);
+            archerCards.Add(cardButton);
+        }
+
+        //Add Skill Card
+        for (int i = 0; i < skillSOList.Count; i++)
+        {
+            GameObject cardButton = Instantiate(skillCardPrefab);
+            cardButton.name = "Skill_Card " + i;
+            cardButton.transform.SetParent(skillContent, false);
+            cardButton.GetComponent<AddValue_Skill>().skillSO = skillSOList[i];
+            skillCards.Add(cardButton);
         }
     }
 }
---
+++ Project D/Assets/MAIN/Scrips/Shop/AddValue_Skill.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddValue_Skill : MonoBehaviour
+{
+    public SO_Active_Skill skillSO;
+
+    [SerializeField]
+    private GameObject imageSkill;
+    [SerializeField]
+    private GameObject typeSkillText; //có thể sẽ pt sau 
+    [SerializeField]
+    private GameObject infoSkillText;
+    [SerializeField]
+    private GameObject manaSkillText;
+    [SerializeField]
+    private GameObject coinText;
+
+
+    private void Start()
+    {
+        imageSkill.GetComponent<Image>().sprite = skillSO.Avatar;
+        infoSkillText.GetComponent<TextMeshProUGUI>().text = skillSO.InforSkill;
+        manaSkillText.GetComponent<TextMeshProUGUI>().text = skillSO.CostSummon.ToString();
+        coinText.GetComponent<TextMeshProUGUI>().text = skillSO.PriceBuy.ToString();
+    }
+}
